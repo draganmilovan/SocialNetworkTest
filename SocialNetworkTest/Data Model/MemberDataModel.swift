d620f85c30c82b516c7b932e24ce3273993b40bf
Dragan Milovanovic <draganmilovan381@gmail.com>
2019-12-19 18:55:27 +0100

    Created Member Data Model

---
+++ SocialNetworkTest/Data Model/MemberDataModel.swift
@@ -0,0 +1,21 @@
+//
+//  MemberDataModel.swift
+//  SocialNetworkTest
+//
+//  Created by Dragan Milovanovic on 12/19/19.
+//  Copyright © 2019 Dragan Milovanovic. All rights reserved.
+//
+
+import Foundation
+
+
+struct MemberDataModel {
+    
+    let id: Int
+    let firstName: String
+    let surname: String?
+    let age: Int?
+    let gender: String?
+    var friends: [String] = []
+    
+}
