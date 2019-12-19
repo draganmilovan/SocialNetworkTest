72d09c3624e26ebbadd1a791289e15b8dfdc4bcf
Dragan Milovanovic <draganmilovan381@gmail.com>
2019-12-19 18:09:10 +0100

    Initial Commit

---
+++ SocialNetworkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialNetworkTest
+//
+//  Created by Dragan Milovanovic on 12/19/19.
+//  Copyright © 2019 Dragan Milovanovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
