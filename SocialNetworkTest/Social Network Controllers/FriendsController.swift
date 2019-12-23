19e30c6caf909bff4252b1938912ebfa9dd3c4bd
Dragan Milovanovic <draganmilovan381@gmail.com>
2019-12-23 11:20:46 +0100

    Added Friends Controller

---
+++ SocialNetworkTest/Social Network Controllers/FriendsController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsController.swift
+//  SocialNetworkTest
+//
+//  Created by Dragan Milovanovic on 12/23/19.
+//  Copyright © 2019 Dragan Milovanovic. All rights reserved.
+//
+
+import UIKit
+
+class FriendsController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
